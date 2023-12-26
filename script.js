1e879cb477bddfdd801b91585f4ad14c12ed836e
SURAJ <77872306+DrSuraj@users.noreply.github.com>
2023-12-26 15:15:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// script.js
+
+// Function to play a sound
+function playSound(soundFileName) {
+    var audio = new Audio('sounds/' + soundFileName);
+    audio.play();
+}
+
+// Function to stop all sounds
+function stopAllSounds() {
+    var allAudioElements = document.querySelectorAll('audio');
+    allAudioElements.forEach(function(audio) {
+        audio.pause();
+        audio.currentTime = 0;
+    });
+}
